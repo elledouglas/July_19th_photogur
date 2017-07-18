d29ce99542734773197f5d9b27c34a00b7872942
Kathleen <kathleenmcclay@gmail.com>
2017-07-18 15:08:17 -0400

    2nd commit

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,13 @@
+class PicturesController < ApplicationController
+
+  def index
+  @pictures = Picture.all
+end
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    render text: "Received POST request to '/pictures' with the data URL: #{params}"
+  end
+   end
